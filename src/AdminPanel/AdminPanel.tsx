49e333cef26340d8da292006143caff40afd9232
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2019-04-22 19:13:41 +0300

    #2 crud: deletion works

--- src/AdminPanel/AdminPanel.tsx
+++ src/AdminPanel/AdminPanel.tsx
@@ -3,20 +3,36 @@ import IngredientList from "./IngredientList";
 import IngredientForm from "./IngredientForm";
 import Ingredient from "../models/ingredient";
 
-export default class AdminPanel extends Component {
-    private list: Ingredient[] = [];
+interface IState {
+    ingredients: Ingredient[];
+}
 
+export default class AdminPanel extends Component<{}, IState> {
     constructor(props: {}) {
         super(props);
-        this.list.push({ name: "Apple", id: 1 });
+
+        this.state = {
+            ingredients: [
+                { name: "Яблоко", id: 1 },
+                { name: "Апельсин", id: 2 },
+                { name: "Чеснок", id: 3 },
+                { name: "Молоко", id: 4 }
+            ]
+        };
     }
 
     public render = () => {
         return (
             <div className="container">
-                <IngredientList ingredients={this.list} />
-                <IngredientForm ingredient={this.list[0]} />
+                <IngredientForm ingredient={this.state.ingredients[0]} />
+                <IngredientList ingredients={this.state.ingredients} onDelete={this.handleDelete} onEdit={() => {}} />
             </div>
         );
+    }
+
+    private handleDelete = (ingredient: Ingredient) => {
+        this.setState({
+            ingredients: [...this.state.ingredients].filter(x => x !== ingredient)
+        });
     }
 }
--- src/AdminPanel/IngredientList.tsx
+++ src/AdminPanel/IngredientList.tsx
@@ -3,6 +3,8 @@ import Ingredient from "../models/ingredient";
 
 export interface IProps {
     ingredients: Ingredient[];
+    onDelete: (ingredient: Ingredient) => any;
+    onEdit: (ingredient: Ingredient) => any;
 }
 
 export default class IngredientList extends Component<IProps, {}> {
@@ -11,6 +13,50 @@ export default class IngredientList extends Component<IProps, {}> {
     }
 
     public render = () => {
-        return <div>I am list {this.props.ingredients.length}</div>;
+        const { ingredients } = this.props;
+
+        return (
+            <table className="table-hover table-bordered w-100">
+                <thead className="thead-dark">
+                    <tr>
+                        <th>ID</th>
+                        <th>Название</th>
+                        <th />
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>{ingredients.map((x, i) => this.renderRow(x, i))}</tbody>
+            </table>
+        );
+    }
+
+    private renderRow = (ingredient: Ingredient, index: number) => {
+        const { onDelete, onEdit } = this.props;
+
+        return (
+            <tr key={index}>
+                <td>{ingredient.id}</td>
+                <td>{ingredient.name}</td>
+                <td>
+                    <button id="btnEdit">
+                        <i className="fas fa-edit" />
+                    </button>
+                </td>
+                <td>
+                    <button id="btnDelete" onClick={() => this.handleDelete(ingredient)}>
+                        <i className="fas fa-trash-alt" />
+                    </button>
+                </td>
+            </tr>
+        );
+    }
+
+    private handleDelete = (ingredient: Ingredient) => {
+        const ok: boolean = confirm("вы уверены?");
+        if (!ok) {
+            return;
+        }
+
+        this.props.onDelete(ingredient);
     }
 }
