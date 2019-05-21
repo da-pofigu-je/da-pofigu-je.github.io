f3304f2b565e13f682d0a6653d72690ab8f36033
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2019-05-21 17:17:24 +0300

    #18 done

--- src/AdminPanel/RecipeList.tsx
+++ src/AdminPanel/RecipeList.tsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import Recipe from "../models/recipe";
-import Ingredient from "../models/ingredient";
 
 export interface IProps {
     recipes: Recipe[];
+    onDelete: (recipe: Recipe) => any;
+    onEdit: (recipe: Recipe) => any;
 }
 
 export default class RecipeList extends Component<IProps, {}> {
@@ -21,24 +22,45 @@ export default class RecipeList extends Component<IProps, {}> {
                         <th>Название</th>
                         <th>Описание</th>
                         <th>Ингредиенты</th>
+                        <th />
                     </tr>
                 </thead>
                 <tbody>{recipes.map((x, i) => this.renderRow(x, i))}</tbody>
             </table>
         );
     }
+
     private renderRow = (recipe: Recipe, index: number) => {
+        const { onEdit } = this.props;
         const ingredientsStr = recipe.ingredients
             .filter(ingredient => ingredient.ingredient !== null)
             .map(ingredient => ingredient.ingredient!.name + " " + ingredient.amount)
             .join(" ; ");
+
         return (
             <tr key={index}>
                 <td>{recipe.id}</td>
                 <td>{recipe.name}</td>
                 <td>{recipe.description}</td>
                 <td>{ingredientsStr}</td>
+                <td>
+                    <button onClick={() => onEdit(recipe)}>
+                        <i className="fas fa-edit" />
+                    </button>
+                    <button onClick={() => this.handleDelete(recipe)}>
+                        <i className="fas fa-trash-alt" />
+                    </button>
+                </td>
             </tr>
         );
+    }
+
+    private handleDelete = (recipe: Recipe) => {
+        const ok: boolean = confirm("вы уверены?");
+        if (!ok) {
+            return;
+        }
+
+        this.props.onDelete(recipe);
     }
 }
