dcfc48af8d390887f456b70dfb952196799c0c17
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2019-05-22 17:45:12 +0300

    #13 test for row

---
+++ src/AdminPanel/Recipes/IngredientAmountRow.test.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import Enzyme, { shallow } from "enzyme";
+import IngredientAmountRow from "./IngredientAmountRow";
+
+// tslint:disable:typedef
+Enzyme.configure({ adapter: new Adapter() });
+
+it("IngredientAmountRow: delete widget calls callback", () => {
+    // assert
+    const deleteCb = jest.fn();
+    const changeCb = jest.fn();
+    const wrapper = shallow(<IngredientAmountRow onDelete={deleteCb} onChange={changeCb} ingredientName={"name"} />);
+    const deleteWidget = wrapper.find("button").first();
+
+    // action
+    deleteWidget.simulate("click");
+
+    // assert
+    expect(deleteCb).toBeCalledTimes(1);
+});
+
+it("IngredientAmountRow: amount input change calls callback", () => {
+    // assert
+    const deleteCb = jest.fn();
+    const changeCb = jest.fn();
+    const wrapper = shallow(<IngredientAmountRow onDelete={deleteCb} onChange={changeCb} ingredientName={"name"} />);
+    const input = wrapper.find("input").first();
+
+    // action
+    input.simulate("change", { target: { value: "96" } });
+
+    // assert
+    expect(changeCb).toBeCalledTimes(1);
+});
