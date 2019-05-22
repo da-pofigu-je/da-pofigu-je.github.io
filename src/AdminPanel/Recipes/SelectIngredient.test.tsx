9af1234ab4a7163a38d47764e5469c9e15149485
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2019-05-22 18:14:57 +0300

    #13 tests for select

--- src/AdminPanel/Recipes/SelectIngredient.test.tsx
+++ src/AdminPanel/Recipes/SelectIngredient.test.tsx
@@ -16,5 +16,32 @@ it("SelectIngredient: displays correct options", () => {
 
     // assert
     expect(wrapper.find("option").length).toBe(1);
-    expect(wrapper.find("option").first().text()).toEqual(name);
+    expect(
+        wrapper
+            .find("option")
+            .first()
+            .text()
+    ).toEqual(name);
+});
+
+it("SelectIngredient: can pass selection to callback", () => {
+    // assign
+    const addCb = jest.fn();
+    const name = "ing";
+    const ingredietns: Ingredient[] = [{ name: "zzz", id: 1 }, { name, id: 2 }];
+    const wrapper = shallow(<SelectIngredient onAdd={addCb} ingredients={ingredietns} />);
+
+    // action
+    wrapper
+        .find("select")
+        .first()
+        .simulate("change", { target: { value: name } });
+    wrapper
+        .find("button")
+        .first()
+        .simulate("click");
+
+    // assert
+    expect(addCb).toBeCalledTimes(1);
+    expect(addCb).toBeCalledWith(ingredietns[1]);
 });
